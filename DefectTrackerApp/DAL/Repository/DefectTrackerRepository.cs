453b117a37b33dcb92adf4905f8428f3207d9a8c
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2024-01-03 15:03:39 +0530

    Add files via upload

---
+++ DefectTrackerApp/DAL/Repository/DefectTrackerRepository.cs
@@ -0,0 +1,61 @@
+﻿using DefectTrackerApp.DAL.Interface;
+using DefectTrackerApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DefectTrackerApp.DAL.Repository
+{
+    public class DefectTrackerRepository : IDefectTrackerRepository
+    {
+        private DefectTrackerDbContext _context;
+        public DefectTrackerRepository(DefectTrackerDbContext Context)
+        {
+            this._context = Context;
+        }
+        public IEnumerable<Defect> GetDefects()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public Defect GetDefectByID(int id)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public Defect InsertDefect(Defect Defect)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public int DeleteDefect(int DefectID)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public bool UpdateDefect(Defect Defect)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public void Save()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+        public void Dispose()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DefectTrackerApp/Models/Defect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DefectTrackerApp.Models
+{
+    public class Defect
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public string DefectName { get; set; }
+        public bool IsCompleted { get; set; }
+        public string Notes { get; set; }
+    }
+
+}
